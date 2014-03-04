34bfd51efc22a6f92d568d09c71b53f2a8c5c6e0
Ray Patterson <ray.patterson@akqa.com>
2014-03-04 11:30:04 -0800

    Updated page.

--- js/demo.js
+++ js/demo.js
@@ -134,3 +134,106 @@ window.Breakpoints = (function(window, document) {
 		return init();
 
 })(window, document);
+;
+(function(window, document, undefined) {
+
+  'use strict';
+
+  // Extract map of all breakpoints and
+  // selectors from the <head> 'font-family'.
+  var getStyle = function(el, styleProp) {
+    if (el.currentStyle) {
+      return el.currentStyle[styleProp];
+    } else if (window.getComputedStyle) {
+      return document.defaultView.getComputedStyle(el, null).getPropertyValue(styleProp);
+    }
+  };
+
+  var map = (getStyle(document.getElementsByTagName('head')[0], 'font-family'))
+    .replace(/'/g, '')
+    .split(' "||" ');
+
+  var breakpoints;
+  var val;
+  var arr;
+  var id;
+  var bp = {};
+  var i = 0;
+  var I = map.length;
+
+  for (i; i < I; i++) {
+
+    val = map[i];
+
+    // Parse breakpoints from map.
+    arr = val.split(' "|" ');
+    breakpoints = arr.pop().replace(/"/g, '').split(' ');
+
+    // Parse selector from map.
+    id = arr.pop().replace(/"/g, '');
+
+    // Use element as key to access breakpoints.
+    bp[id] = breakpoints;
+  }
+
+  // Create interface.
+  var BreakpointBridge = window.BreakpointBridge || {};
+
+  BreakpointBridge.activate = function(id, el, matched, exit) {
+
+    // Un-$-ify.
+    if (el instanceof $) {
+      el = el[0];
+    }
+
+    breakpoints = bp[id];
+
+    if (breakpoints !== undefined) {
+
+      // Iterate though breakpoints and apply event handlers.
+      $.each(breakpoints, function(key, val) {
+        Breakpoints.on({
+          name: val,
+          el: el,
+          matched: matched,
+          exit: exit
+        });
+      });
+    } else {
+
+      // Breakpoints are undefined for this element.
+    }
+  };
+
+  // Expose interface.
+  window.BreakpointBridge = BreakpointBridge;
+
+}(this, this.document));
+;
+(function($, window, document, undefined) {
+
+  'use strict';
+
+  var $el;
+
+  var matched = function() {
+    console.log("Breakpoint : matched", this);
+
+    $el = $(this.el);
+    $el.addClass(this.name);
+    $el.children('.debug').html(this.name);
+  };
+
+  var exit = function() {
+    console.log("Breakpoint : exit", this);
+
+    $el = $(this.el);
+    $el.removeClass(this.name);
+  };
+
+  window.BreakpointBridge.activate('body', $('body')[0], matched, exit);
+  window.BreakpointBridge.activate('header', $('.header'), matched, exit);
+  window.BreakpointBridge.activate('aside', $('.aside'), matched, exit);
+  window.BreakpointBridge.activate('article', $('.article'), matched, exit);
+
+}(this.jQuery, this, this.document));
