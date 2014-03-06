61a6e78b1b191379c4871a35535a548e6acc5c6f
Ray Patterson <ray.patterson@akqa.com>
2014-03-05 22:13:48 -0800

    Updated page.

--- js/demo.js
+++ js/demo.js
@@ -210,6 +210,12 @@ window.Breakpoints = (function(window, document) {
 
 }(this, this.document));
 ;
+
+/**
+ * Updating content.
+ * Adding and removing a CSS class.
+ */
+
 (function($, window, document, undefined) {
 
   'use strict';
@@ -217,6 +223,7 @@ window.Breakpoints = (function(window, document) {
   var $el;
 
   var matched = function() {
+
     console.log("Breakpoint : matched", this);
 
     $el = $(this.el);
@@ -225,6 +232,7 @@ window.Breakpoints = (function(window, document) {
   };
 
   var exit = function() {
+
     console.log("Breakpoint : exit", this);
 
     $el = $(this.el);
@@ -235,5 +243,54 @@ window.Breakpoints = (function(window, document) {
   window.BreakpointBridge.activate('header', $('.header'), matched, exit);
   window.BreakpointBridge.activate('aside', $('.aside'), matched, exit);
   window.BreakpointBridge.activate('article', $('.article'), matched, exit);
+
+}(this.jQuery, this, this.document));
+
+/**
+ * Enabling and disabling a process.
+ */
+
+(function($, window, document, undefined) {
+
+  'use strict';
+
+  var $el = $('.article .process');
+  var $debug = $el.children('.debug');
+  var count = 0;
+
+  var process;
+
+  var startProcess = function() {
+
+    $debug.html('process executing : ' + (count++));
+
+    process = setTimeout(startProcess, 2000);
+  };
+
+  var stopProcess = function() {
+
+    $debug.html('process stopped : ' + count);
+
+    clearTimeout(process);
+  };
+
+  window.BreakpointBridge.activate('process', $el, function() {
+
+    console.log("Breakpoint : matched", this);
+
+    if (this.name.match(/disable/gi)) {
+
+      stopProcess();
+
+    } else if (this.name.match(/enable/gi)) {
+
+      startProcess();
+    }
+
+  }, function() {
+
+    console.log("Breakpoint : exit", this);
+
+  });
 
 }(this.jQuery, this, this.document));
