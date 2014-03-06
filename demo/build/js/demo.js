23618e4ce668996f82f8969a7c51e0433fd3a1b0
Ray Patterson <ray.patterson@akqa.com>
2014-03-05 18:33:03 -0800

    Demonstrated how to enable and disable a process.

--- demo/build/js/demo.js
+++ demo/build/js/demo.js
@@ -210,6 +210,12 @@ window.Breakpoints = (function(window, document) {
 
 }(this, this.document));
 ;
+
+/**
+ * Updating content.
+ * Adding and removing a CSS class.
+ */
+
 (function($, window, document, undefined) {
 
   'use strict';
@@ -217,6 +223,7 @@ window.Breakpoints = (function(window, document) {
   var $el;
 
   var matched = function() {
+
     console.log("Breakpoint : matched", this);
 
     $el = $(this.el);
@@ -225,6 +232,7 @@ window.Breakpoints = (function(window, document) {
   };
 
   var exit = function() {
+
     console.log("Breakpoint : exit", this);
 
     $el = $(this.el);
@@ -235,5 +243,54 @@ window.Breakpoints = (function(window, document) {
   window.BreakpointBridge.activate('header', $('.header'), matched, exit);
   window.BreakpointBridge.activate('aside', $('.aside'), matched, exit);
   window.BreakpointBridge.activate('article', $('.article'), matched, exit);
+
+}(this.jQuery, this, this.document));
+
+/**
+ * Enabling and disabling a process.
+ */
+
+(function($, window, document, undefined) {
+
+  'use strict';
+
+  var process;
+
+  var startProcess = function() {
+
+    console.log('process begin');
+
+    process = setInterval(function() {
+
+      console.log('process complete');
+
+    }, 2000);
+  };
+
+  var stopProcess = function() {
+
+    console.log('process stop');
+
+    clearInterval(process);
+  };
+
+  window.BreakpointBridge.activate('process', $('.article .process'), function() {
+
+    console.log("Breakpoint : matched", this);
+
+    if (this.name.match(/disable/gi)) {
+
+      stopProcess();
+
+    } else if (this.name.match(/enable/gi)) {
+
+      startProcess();
+    }
+
+  }, function() {
+
+    console.log("Breakpoint : exit", this);
+
+  });
 
 }(this.jQuery, this, this.document));
--- demo/source/js/demo.js
+++ demo/source/js/demo.js
@@ -1,4 +1,10 @@
 ;
+
+/**
+ * Updating content.
+ * Adding and removing a CSS class.
+ */
+
 (function($, window, document, undefined) {
 
   'use strict';
@@ -6,6 +12,7 @@
   var $el;
 
   var matched = function() {
+
     console.log("Breakpoint : matched", this);
 
     $el = $(this.el);
@@ -14,6 +21,7 @@
   };
 
   var exit = function() {
+
     console.log("Breakpoint : exit", this);
 
     $el = $(this.el);
@@ -24,5 +32,54 @@
   window.BreakpointBridge.activate('header', $('.header'), matched, exit);
   window.BreakpointBridge.activate('aside', $('.aside'), matched, exit);
   window.BreakpointBridge.activate('article', $('.article'), matched, exit);
+
+}(this.jQuery, this, this.document));
+
+/**
+ * Enabling and disabling a process.
+ */
+
+(function($, window, document, undefined) {
+
+  'use strict';
+
+  var process;
+
+  var startProcess = function() {
+
+    console.log('process begin');
+
+    process = setInterval(function() {
+
+      console.log('process complete');
+
+    }, 2000);
+  };
+
+  var stopProcess = function() {
+
+    console.log('process stop');
+
+    clearInterval(process);
+  };
+
+  window.BreakpointBridge.activate('process', $('.article .process'), function() {
+
+    console.log("Breakpoint : matched", this);
+
+    if (this.name.match(/disable/gi)) {
+
+      stopProcess();
+
+    } else if (this.name.match(/enable/gi)) {
+
+      startProcess();
+    }
+
+  }, function() {
+
+    console.log("Breakpoint : exit", this);
+
+  });
 
 }(this.jQuery, this, this.document));
